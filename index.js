192f20c715c228fd0d0a9003ccd351b202c8658c
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2022-08-06 09:07:51 -0300

    aprofundando na API

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const express = require('express');
 const app = express();
 
 app.listen('3000')
+app.use(express.json()); // precisa para tratar o JSon de retorno nos métodos
 
 app.route('/').get((req, res) => {
     res.send('olá');
 });
 
 app.route('/').post((req,res) => {
+    console.log('body -------------------------');
     console.log(req.body);
+    console.log('baseUrl -------------------------');
     console.log(req.baseUrl)
-    console.log(req.body)
+    console.log('headers -------------------------');
     console.log(req.headers)
+    res.body = 'teste ronald';
 });
+
+app.route('/about').get((req,res) => res.send('Página sobre'));
+
+app.route('/about').post((req,res) => {
+    const {nome, email, conhecimentos} = req.body
+    let retorno = `BODY ---------\n`
+    retorno = retorno + `Nome: ${nome}\n`
+    retorno = retorno + `E-mail: ${email}\n`
+    retorno = retorno + `Conhecimentos: ${conhecimentos}\n`
+    retorno = retorno + `\n\nBASE URL => ${req.baseUrl}`
+    
+    return res.send(retorno);
+})
