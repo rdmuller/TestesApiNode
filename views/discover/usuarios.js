75ba4efc85b566ff271c8198cfad6df5e38c99ec
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2022-08-24 07:26:18 -0300

    chamada sincrona

--- views/discover/usuarios.js
+++ views/discover/usuarios.js
@@ -1,7 +1,32 @@
 const axios = require('axios');
 const urlDiscover = 'http://localhost:5500/api'
 
-const renderView = (res) => {
+const renderView = async (res) => {
+    try {
+        const result = await axios.get(urlDiscover)
+        const users = result.data['users']
+        listUsersConsole(users)
+
+        res.render('discover/index', {
+            result : JSON.stringify(result.data),
+            users : users,
+        })
+    }
+    catch (e) {
+        console.error(e)
+    }
+    finally {
+        console.log("Fim")
+    }
+}
+
+function listUsersConsole(users) {
+    for(let i = 0; i < users.length; i++){
+        console.log(users[i].name)
+    }
+}
+
+/*const renderView = (res) => {
     axios.get(urlDiscover)
     .then(result => {
         const users = result.data['users']
@@ -14,7 +39,7 @@ const renderView = (res) => {
         })
     })
     .catch(error => console.error(error))
-}
+}*/
 
 module.exports = {
     renderView,
